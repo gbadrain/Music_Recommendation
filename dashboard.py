2509d93f61b88371bc61f14cdc705d60589f0f88
Gurpreet <gbadrain@gmail.com>
2025-06-07 03:31:04 -0700

    Issue slideshow-streamlit

--- dashboard.py
+++ dashboard.py
@@ -431,7 +431,9 @@ def show_slideshow():
 """, unsafe_allow_html=True)
 
     # Load images
-    slides_dir = "/Users/GURU/Desktop/Music_Recommendation/Slides/"
+    # slides_dir = "/Users/GURU/Desktop/Music_Recommendation/Slides/"
+    slides_dir = os.path.join(SCRIPT_DIR, "Slides")
+
     try:
         slides = sorted(
             [f for f in os.listdir(slides_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg'))],
